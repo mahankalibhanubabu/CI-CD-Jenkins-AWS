1e24227263d7ed17b55515a5f459857e4c3728bd
mahankalibhanubabu <mahankalibhanubabu232323@gmail.com>
2026-08-17 06:03:01 -0400

    feat: initialize Next.js home page component

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Farzeen {" "}
+            bhanu {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/me.tsx
             </code>
@@ -23,6 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
